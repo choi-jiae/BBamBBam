389a16851e051f6371bde5ef3714824bb5c114f3
choi-jiae <96973901+choi-jiae@users.noreply.github.com>
2024-04-18 17:33:12 +0900

    Add background image and button

--- bbambbam/lib/home.dart
+++ bbambbam/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,40 +12,70 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container( color: Colors.white,
-        child: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                width: 300,
-                height: 100,
-                child: Text(
-                  'BBAM BBAM',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.blueAccent,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold),
-                ),
-              ),
-              SizedBox(
-                width: 350,
-                height: 80,
-                child: Text(
-                  'Welcome to BBAM BBAM',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.blueAccent,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: const IconButton(
+          icon: Icon(Icons.menu, color: Colors.white, size: 30),
+          onPressed: null // hamburger menu 연결하기
           ),
-        ),
+          title: const Text('BBAM BBAM', 
+            style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+            ),
       ),
+      body: Stack(
+        children: <Widget>[
+          Image.asset('assets/images/background.png',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+            ),
+          Positioned(
+            top: 340,
+            left: 65,
+            child: GestureDetector(
+              onTap: () {
+                
+              },
+              child: Image.asset(
+                'assets/images/handle.png',
+                width: 300,
+                height: 300,
+              ),
+            ),
+            )
+            
+        ],
+      ),
+      bottomSheet: Container(
+        height:100,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          )
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 5,
+              width: 50,
+              margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(15),
+              ),
+
+            ),
+            Center(child: Text('bottom sheet'),)
+          ],)
+        
+      ),
+
     );
   }
 }
