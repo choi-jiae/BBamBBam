69e881c572b815b7041c4385439032370caccd91
shhyunn <limgm7@gmail.com>
2024-04-29 22:00:44 +0900

    connect hamburger bar

--- bbambbam/lib/home.dart
+++ bbambbam/lib/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:bbambbam/driving.dart';
 import 'package:bbambbam/report.dart';
+import 'package:bbambbam/sidetap.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,7 +14,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-   void _showFullBottomSheet(BuildContext context) {
+  void _showFullBottomSheet(BuildContext context) {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
@@ -31,22 +32,26 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.transparent,
         elevation: 0.0,
         centerTitle: true,
-        leading: const IconButton(
-          icon: Icon(Icons.menu, color: Colors.white, size: 30),
-          onPressed: null // hamburger menu 연결하기
-          ),
-          title: const Text('BBAM BBAM', 
-            style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
-            ),
+        // leading: const IconButton(
+        //     icon: Icon(Icons.menu, color: Colors.white, size: 30),
+        //     onPressed: null // hamburger menu 연결하기
+        //     ),
+        title: const Text(
+          'BBAM BBAM',
+          style: TextStyle(
+              color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+        ),
       ),
+      drawer: Sidetap(),
       body: Stack(
         children: <Widget>[
-          Image.asset('assets/images/background.png',
+          Image.asset(
+            'assets/images/background.png',
             fit: BoxFit.cover,
             height: double.infinity,
             width: double.infinity,
             alignment: Alignment.center,
-            ),
+          ),
           Positioned(
             top: 340,
             left: 65,
@@ -54,8 +59,7 @@ class _HomeState extends State<Home> {
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                      builder: (context) => const Driving()),
+                  MaterialPageRoute(builder: (context) => const Driving()),
                 );
               },
               child: Image.asset(
@@ -64,51 +68,51 @@ class _HomeState extends State<Home> {
                 height: 300,
               ),
             ),
-            )
-            
+          )
         ],
       ),
-      bottomSheet: 
-      GestureDetector(
+      bottomSheet: GestureDetector(
         onTap: () => _showFullBottomSheet(context),
         onVerticalDragUpdate: (details) {
-          if (details.delta.dy < 0) { // 위로 swipe
+          if (details.delta.dy < 0) {
+            // 위로 swipe
             _showFullBottomSheet(context);
           }
         },
-        child: 
-      Container(
-        height:100,
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
-          )
-        ),
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 5,
-              width: 50,
-              margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(15),
-              ),
-
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: 20, top: 10),
-                child: Text('📑 운전 리포트', style: TextStyle(color:Colors.black54, fontSize: 25, fontWeight: FontWeight.bold),),) 
-              )
-          ],)
-        
+        child: Container(
+            height: 100,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                )),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 5,
+                  width: 50,
+                  margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 20, top: 10),
+                      child: Text(
+                        '📑 운전 리포트',
+                        style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ))
+              ],
+            )),
       ),
-      ),
-
     );
   }
 }
