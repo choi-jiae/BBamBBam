866f6110290ed8b5ef6c6df77e46e4f89e4353ea
Lim Sohyun <108120508+shhyunn@users.noreply.github.com>
2024-07-27 14:36:07 +0900

    Feat: add repot data to firebase datastore (#13)

--- bbambbam/lib/pages/driving/driving.dart
+++ bbambbam/lib/pages/driving/driving.dart
@@ -12,7 +12,6 @@ import 'detector_view.dart';
 import 'painters/face_mesh_detector_painter.dart';
 import 'dart:math';
 
-
 class Driving extends StatefulWidget {
   const Driving({super.key});
 
@@ -21,26 +20,50 @@ class Driving extends StatefulWidget {
 }
 
 class _DrivingState extends State<Driving> {
-  final FaceMeshDetector _meshDetector = 
-    FaceMeshDetector(option: FaceMeshDetectorOptions.faceMesh);
+  late String uid;
+  late DateTime nowDate;
+  late String formattedDate;
+  Map<String, dynamic> drivingRecord = {
+    'date': '2023-10-12',
+    'count': 0, // 운전 횟수
+    'timestamp': [], // 운전 시간 (분)
+    'total': '00:00:00',
+  };
+
+  final FaceMeshDetector _meshDetector =
+      FaceMeshDetector(option: FaceMeshDetectorOptions.faceMesh);
   bool _canProcess = true;
   bool _isBusy = false;
   int _sleepCount = 0;
-  String _sleepTimes = '';
+  final List<String> _sleepTimes = [];
   DateTime? _eyesClosedStartTime;
   CustomPaint? _customPaint;
   String? _text;
   var _cameraLensDirection = CameraLensDirection.front;
 
   @override
+  void initState() {
+    super.initState();
+    uid = FirebaseAuth.instance.currentUser!.uid;
+    nowDate = DateTime.now();
+    formattedDate = DateFormat('yyyy-MM-dd').format(nowDate);
+    drivingRecord['date'] = formattedDate;
+  }
+
+  @override
   void dispose() {
     _canProcess = false;
+    _handleDispose();
     _meshDetector.close();
     super.dispose();
   }
 
+  Future<void> _handleDispose() async {
+    await addDrivingRecord(uid, drivingRecord);
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return DetectorView(
       title: 'Face Mesh Detection',
       customPaint: _customPaint,
@@ -51,23 +74,45 @@ class _DrivingState extends State<Driving> {
     );
   }
 
+  Future<void> addDrivingRecord(
+      String uid, Map<String, dynamic> drivingRecord) async {
+    try {
+      DocumentReference userReportRef =
+          FirebaseFirestore.instance.collection('Report').doc(uid);
+
+      await userReportRef.set({
+        'drivingRecords': FieldValue.arrayUnion([drivingRecord]),
+        'lastUpdated': FieldValue.serverTimestamp(),
+      }, SetOptions(merge: true));
+
+      print('Driving record added for user: $uid');
+    } catch (e) {
+      print('Error adding driving record: $e');
+    }
+  }
+
 // 두 점 사이의 거리 계산
-  num euclideanDistance(num x1, num y1, num x2, num y2){
+  num euclideanDistance(num x1, num y1, num x2, num y2) {
     var point1 = Point(x1, y1);
     var point2 = Point(x2, y2);
     return point1.distanceTo(point2);
+  }
+
+  void setDrivingRecord(List<String> timeStamp, num count) {
+    drivingRecord['timestamp'] = timeStamp;
+    drivingRecord['count'] = count;
   }
 
   Future<void> _processImage(InputImage inputImage) async {
     if (!_canProcess) return;
     if (_isBusy) return;
     _isBusy = true;
-    setState((){
+    setState(() {
       _text = '';
     });
     final meshes = await _meshDetector.processImage(inputImage);
     if (inputImage.metadata?.size != null &&
-        inputImage.metadata?.rotation != null){
+        inputImage.metadata?.rotation != null) {
       final painter = FaceMeshDetectorPainter(
         meshes,
         inputImage.metadata!.size,
@@ -76,320 +121,73 @@ class _DrivingState extends State<Driving> {
       );
       _customPaint = CustomPaint(painter: painter);
 
-      var leftEAR;
-      var rightEAR;
+      double leftEAR = 0.0;
+      double rightEAR = 0.0;
 
-      
       // 눈 좌표
-      for (final mesh in meshes){
-        List<FaceMeshPoint>? leftEyePoints = mesh.contours[FaceMeshContourType.leftEye];
-        List<FaceMeshPoint>? rightEyePoints = mesh.contours[FaceMeshContourType.rightEye];
+      for (final mesh in meshes) {
+        List<FaceMeshPoint>? leftEyePoints =
+            mesh.contours[FaceMeshContourType.leftEye];
+        List<FaceMeshPoint>? rightEyePoints =
+            mesh.contours[FaceMeshContourType.rightEye];
 
-        if (leftEyePoints != null){
-
-        // 계산식 약간 다름(ear 식 참고)
-          var leftEar1 = euclideanDistance(leftEyePoints[3].x, leftEyePoints[3].y, leftEyePoints[13].x, leftEyePoints[13].y);
-          var leftEar2 = euclideanDistance(leftEyePoints[5].x, leftEyePoints[5].y, leftEyePoints[11].x, leftEyePoints[11].y);
-          var leftEar3 = euclideanDistance(leftEyePoints[0].x, leftEyePoints[0].y, leftEyePoints[8].x, leftEyePoints[8].y);
+        if (leftEyePoints != null) {
+          // 계산식 약간 다름(ear 식 참고)
+          var leftEar1 = euclideanDistance(leftEyePoints[3].x,
+              leftEyePoints[3].y, leftEyePoints[13].x, leftEyePoints[13].y);
+          var leftEar2 = euclideanDistance(leftEyePoints[5].x,
+              leftEyePoints[5].y, leftEyePoints[11].x, leftEyePoints[11].y);
+          var leftEar3 = euclideanDistance(leftEyePoints[0].x,
+              leftEyePoints[0].y, leftEyePoints[8].x, leftEyePoints[8].y);
 
           leftEAR = (leftEar1 + leftEar2) / (2.0 * leftEar3);
         }
 
-        if (rightEyePoints != null){
-          var rightEar1 = euclideanDistance(rightEyePoints[3].x, rightEyePoints[3].y, rightEyePoints[13].x, rightEyePoints[13].y);
-          var rightEar2 = euclideanDistance(rightEyePoints[5].x, rightEyePoints[5].y, rightEyePoints[11].x, rightEyePoints[11].y);
-          var rightEar3 = euclideanDistance(rightEyePoints[0].x, rightEyePoints[0].y, rightEyePoints[8].x, rightEyePoints[8].y);
+        if (rightEyePoints != null) {
+          var rightEar1 = euclideanDistance(rightEyePoints[3].x,
+              rightEyePoints[3].y, rightEyePoints[13].x, rightEyePoints[13].y);
+          var rightEar2 = euclideanDistance(rightEyePoints[5].x,
+              rightEyePoints[5].y, rightEyePoints[11].x, rightEyePoints[11].y);
+          var rightEar3 = euclideanDistance(rightEyePoints[0].x,
+              rightEyePoints[0].y, rightEyePoints[8].x, rightEyePoints[8].y);
 
           rightEAR = (rightEar1 + rightEar2) / (2.0 * rightEar3);
         }
 
-      
         // 눈 감김 여부 판단
-        if (leftEAR != null && rightEAR != null){
-          var ear = (leftEAR + rightEAR) / 2.0;
-          if (ear < 0.2){
-            print("Eyes are closed");
-            _eyesClosedStartTime ??= DateTime.now();
-            
-          } else{
-            print("Eyes are open");
-            _eyesClosedStartTime = null;
-            
-          }
+        var ear = (leftEAR + rightEAR) / 2.0;
+        if (ear < 0.2) {
+          _eyesClosedStartTime ??= DateTime.now();
+        } else {
+          _eyesClosedStartTime = null;
         }
       }
 
-      if (_eyesClosedStartTime != null){
+      if (_eyesClosedStartTime != null) {
         var nowTime = DateTime.now();
         var duration = nowTime.difference(_eyesClosedStartTime!);
-        if (duration.inSeconds > 0.5){
+        if (duration.inSeconds > 0.5) {
           print("졸음 운전 감지");
           _eyesClosedStartTime = null;
           _sleepCount += 1;
-          _sleepTimes += '${DateFormat('HH:mm:ss').format(nowTime)},';
+          _sleepTimes.add(DateFormat('HH:mm:ss').format(nowTime));
+          setDrivingRecord(_sleepTimes, _sleepCount);
           print(_sleepCount);
           print(_sleepTimes);
         }
       }
     } else {
       String text = 'Face meshes found: ${meshes.length}\n\n';
-      for (final mesh in meshes){
+      for (final mesh in meshes) {
         text += 'face: ${mesh.boundingBox}\n\n';
       }
-       _text = text;
+      _text = text;
       // TODO: set _customPaint to draw boundingRect on top of image
       _customPaint = null;
     }
     _isBusy = false;
-    if (mounted){
+    if (mounted) {
       setState(() {});
     }
   }
 }
-
-// class _DrivingState extends State<Driving> with WidgetsBindingObserver{
-  // CameraController? _cameraController;
-  // late List<CameraDescription> _cameras;
-  // late CameraDescription _cameraDescription;
-
-  // late bool _isRun;
-  // bool _predicting = false;
-  // bool _draw = false;
-
-  // late IsolateUtils _isolateUtils;
-  // late ModelInferenceService _modelInferenceService;
-
-  // late User _user;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _modelInferenceService = locator<ModelInferenceService>();
-  //   _initStateAsync();
-  //   _getUser();
-  // }
-
-  // void _initStateAsync() async {
-  //   _isolateUtils = IsolateUtils();
-  //   await _isolateUtils.initIsolate();
-  //   await _initCamera();
-  //   _predicting = false;
-  // }
-
-  // @override
-  // void dispose() {
-  //   _cameraController?.dispose();
-  //   _cameraController = null;
-  //   _isolateUtils.dispose();
-  //   _modelInferenceService.inferenceResults = null;
-  //   super.dispose();
-  // }
-
-  // Future<void> _initCamera() async {
-  //   _cameras = await availableCameras();
-  //   _cameraDescription = _cameras[1];
-  //   _isRun = false;
-  //   _onNewCameraSelected(_cameraDescription);
-  // }
-
-  // void _onNewCameraSelected(CameraDescription cameraDescription) async {
-  //   _cameraController = CameraController(
-  //     cameraDescription,
-  //     ResolutionPreset.medium,
-  //     enableAudio: false,
-  //   );
-
-  //   _cameraController!.addListener(() {
-  //     if (mounted) setState(() {});
-  //     if (_cameraController!.value.hasError){
-  //       _showInSnackBar(
-  //         'Camera error ${_cameraController!.value.errorDescription}'
-  //       );
-  //     }
-  //   });
-
-  //   try {
-  //     await _cameraController!.initialize().then((value){
-  //       if (!mounted) return;
-  //     });
-  //   } on CameraException catch (e){
-  //     _showInSnackBar('Error: ${e.code}\n${e.description}');
-  //   }
-
-  //   if (mounted){
-  //     setState(() {});
-  //   }
-  // }
-
-  // void _showInSnackBar(String message){
-  //   ScaffoldMessenger.of(context).showSnackBar(
-  //     SnackBar(
-  //       content: Text(message),
-  //     ),
-  //   );
-  // }
-
-
-  // Future<void> _getUser() async {
-  //   final user = FirebaseAuth.instance.currentUser;
-  //   if (user != null) {
-  //     setState(() {
-  //       _user = user;
-  //     });
-  //   }
-  // }
-
-  // Future<void> _addReport() async {
-  //   CollectionReference reports = FirebaseFirestore.instance
-  //       .collection('User')
-  //       .doc(_user.uid)
-  //       .collection('Reports');
-  //   await reports.add({
-  //     'warning': true,
-  //     'date': '2022-01-02',
-  //     'start_driving_time': '00:00:00',
-  //     'end_driving_time': '00:00:00',
-  //     'time_stamp': '00:00:00, 00:00:00, 00:00:00',
-  //   });
-  // }
-
-
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return PopScope(
-  //     canPop: true,
-  //     onPopInvoked: (didPop){
-  //       if (_isRun){
-  //         _imageStreamToggle;
-  //       }
-  //     },
-  //     child: Scaffold(
-  //       backgroundColor: Colors.black,
-  //       body: ModelCameraPreview(
-  //         cameraController: _cameraController,
-  //         draw: _draw,
-  //         ),
-  //         floatingActionButton: _buildFloatingActionButton,
-  //         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-  //       ),
-  //   );
-    // return Scaffold(
-    //   body: FutureBuilder<void>(
-    //     future: _initializeControllerFuture,
-    //     builder: (context, snapshot) {
-    //       if (snapshot.connectionState == ConnectionState.done) {
-    //         return Stack(
-    //           children: <Widget>[
-    //             CameraPreview(_controller),
-    //             Positioned(
-    //               left: 0,
-    //               bottom: 0,
-    //               child: FloatingActionButton(
-    //                   child: Icon(Icons.stop),
-    //                   onPressed: () => handleStopClick(context)
-    //                       ),
-    //             ),
-    //             Positioned(
-    //               right: 0,
-    //               bottom: 0,
-    //               child: FloatingActionButton(
-    //                 child: Icon(Icons.pause),
-    //                 onPressed: () {
-    //                   _controller.pauseVideoRecording();
-    //                 },
-    //               ),
-    //             ),
-    //           ],
-    //         );
-    //       } else {
-    //         return const Center(child: CircularProgressIndicator());
-    //       }
-    //     },
-    //   ),
-    // );
-  // }
-
-  // Row get _buildFloatingActionButton => Row(
-  //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //   children: [
-  //     IconButton(
-  //       onPressed: () => handleStopClick(context),
-  //       color: Colors.white,
-  //       iconSize: ScreenUtil().setWidth(30.0),
-  //       icon: const Icon(
-  //         Icons.stop,
-  //       ),
-  //     ),
-  //     // 일단 model test를 위해 모델 실행 버튼으로 대체
-  //     IconButton(
-  //       onPressed: () => _imageStreamToggle,
-  //       color: Colors.white,
-  //       iconSize: ScreenUtil().setWidth(30.0),
-  //       icon: const Icon(
-  //         Icons.filter_center_focus,
-  //         ),
-  //       ),
-  //   ],
-  //   );
-  
-  //   void handleStopClick(BuildContext context) {
-  //   try {
-  //     print('Stop Clicked');
-  //     _addReport();
-  //     Navigator.of(context).pop();
-  //   } catch (e) {
-  //     print('Error: $e');
-  //   }
-  // }
-
-
-  // void get _imageStreamToggle {
-  //   setState((){
-  //     _draw = !_draw;
-  //   });
-
-  //   _isRun = !_isRun;
-  //   if (!_isRun) {
-  //     print("_____stop image stream_____");
-  //   }
-  //   if (_isRun) {
-  //     print("_____start image stream_____");
-  //     _cameraController!.startImageStream(
-  //       (CameraImage cameraImage) async =>
-  //         await _inference(cameraImage: cameraImage),
-  //     );
-  //   } else {
-  //     _cameraController!.stopImageStream();
-  //   }
-  // }
-
-  // Future<void> _inference({required CameraImage cameraImage}) async {
-  //   if (!mounted) return;
-
-  //   if (_modelInferenceService.model.getInterpreter != null){
-  //     if (_predicting || !_draw) {
-  //       return;
-  //     }
-
-  //     setState(() {
-  //       _predicting = true;
-  //     });
-
-  //     if (_draw){
-  //       //print("_____start inference_____");
-  //       await _modelInferenceService.inference(
-  //         isolateUtils: _isolateUtils,
-  //         cameraImage: cameraImage,
-  //       );
-  //     }
-
-  //     setState((){
-  //       _predicting = false;
-  //     });
-  //     print("_____end inference_____");
-
-  //     }
-  //   }
-  // }
-
